c30853b5ba57bb06a72e33262facabe243d6ddee
Fernando Cardoso <fernandohbc@gmail.com>
2011-05-09 19:28:33 -0300

    Removed misincluded old license comments.

--- uvaonlinejudge/src/volume_ii/P272_fernandohbc.cpp
+++ uvaonlinejudge/src/volume_ii/P272_fernandohbc.cpp
@@ -18,30 +18,6 @@
  * If you find this really useful, you can buy a kid a book, and help making
  * the world more literate.
  ******************************************************************************/
-/*
- * P272_fernandohbc.cpp
- * 272 - TEX Quotes
- *
- * Author: Fernando Cardoso (fernandohbc@gmail.com / @fernando_hbc)
- *
- * nandouva - My ACM/UVa programming project
- * Copyright (C) 2007 - 2011  Fernando Cardoso
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- */
 
 #include <string>
 #include <iostream>
--- uvaonlinejudge/src/volume_vi/P623_3747.java
+++ uvaonlinejudge/src/volume_vi/P623_3747.java
@@ -18,95 +18,97 @@
  * If you find this really useful, you can buy a kid a book, and help making
  * the world more literate.
  */
-//623 - 500!
+
 package volume_vi;
 
 import java.util.Scanner;
 
 public class P623_3747 {
-	
-	public static void main(String[] args) {
-		new P623_3747().begin();
-	}
 
-	private void begin() {
-		BigNum factorials[] = new BigNum[1001];
-		factorials[0] = new BigNum(1);
-		for ( int i = 1; i <= 1000; i++ ) {
-			factorials[i] = factorials[i-1].times(i);
-		}
-		Scanner scn = new Scanner(System.in);
-		while ( scn.hasNextInt() ) {
-			int n = scn.nextInt();
-			System.out.println(n + "!");
-			System.out.println(factorials[n]);
-		}
-	}
+    public static void main(String[] args) {
+        new P623_3747().begin();
+    }
+
+    private void begin() {
+        BigNum factorials[] = new BigNum[1001];
+        factorials[0] = new BigNum(1);
+        for (int i = 1; i <= 1000; i++) {
+            factorials[i] = factorials[i - 1].times(i);
+        }
+        Scanner scn = new Scanner(System.in);
+        while (scn.hasNextInt()) {
+            int n = scn.nextInt();
+            System.out.println(n + "!");
+            System.out.println(factorials[n]);
+        }
+    }
 }
 
 class BigNum {
-	private byte[] digits;
-	private int size;
-	
-	public BigNum(int i) {
-		int cap = (int) Math.round(Math.log10(i) + 1);
-		size = 0;
-		this.digits = new byte[cap];
-		while ( i != 0 ) {
-			digits[size++] = (byte) (i%10);
-			i = i/10;
-		}
-	}
+    private byte[] digits;
+    private int size;
 
-	private BigNum() {}
+    public BigNum(int i) {
+        int cap = (int) Math.round(Math.log10(i) + 1);
+        this.size = 0;
+        this.digits = new byte[cap];
+        while (i != 0) {
+            this.digits[this.size++] = (byte) (i % 10);
+            i = i / 10;
+        }
+    }
 
-	public BigNum times(int n) {
-		BigNum result = new BigNum();
-		result.setCap(this.getSize() + (int) Math.round(Math.log10(n)) + 1);
-		int carry = 0;
-		int i = 0;
-		for (i = 0 ; i < this.getSize(); i++  ) {			
-			int aux = this.digits[i] * n + carry;
-			result.setDigit(i, (byte) (aux%10));
-			carry = aux/10;
-		}
-		int j = i;
-		while ( carry != 0 ) {
-			result.setDigit(j, (byte) (carry%10));
-			j++;
-			carry = carry/10;
-		}
-		result.setSize(j);
-		return result;
-	}
+    private BigNum() {
+    }
 
-	private void setSize(int j) {
-		this.size = j;
-	}
+    public BigNum times(int n) {
+        BigNum result = new BigNum();
+        result.setCap(this.getSize() + (int) Math.round(Math.log10(n)) + 1);
+        int carry = 0;
+        int i = 0;
+        for (i = 0; i < this.getSize(); i++) {
+            int aux = this.digits[i] * n + carry;
+            result.setDigit(i, (byte) (aux % 10));
+            carry = aux / 10;
+        }
+        int j = i;
+        while (carry != 0) {
+            result.setDigit(j, (byte) (carry % 10));
+            j++;
+            carry = carry / 10;
+        }
+        result.setSize(j);
+        return result;
+    }
 
-	private void setDigit(int i, byte j) {
-		digits[i] = j;		
-	}
+    private void setSize(int j) {
+        this.size = j;
+    }
 
-	private int getSize() {
-		return size;
-	}
+    private void setDigit(int i, byte j) {
+        this.digits[i] = j;
+    }
 
-	private void setCap(int cap) {
-		byte[] aux = digits;
-		digits = new byte[cap];
-		if ( aux != null ) {			
-			for ( int i = 0; i < aux.length; i++ ) {
-				digits[i] = aux[i];
-			}
-		}
-	}
+    private int getSize() {
+        return this.size;
+    }
 
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = this.size - 1; i >= 0; i-- ) {
-			sb.append(digits[i]);
-		}
-		return sb.toString();
-	}
+    private void setCap(int cap) {
+        byte[] aux = this.digits;
+        this.digits = new byte[cap];
+        if (aux != null) {
+            for (int i = 0; i < aux.length; i++) {
+                this.digits[i] = aux[i];
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = this.size - 1; i >= 0; i--) {
+            sb.append(this.digits[i]);
+        }
+        return sb.toString();
+    }
 }
